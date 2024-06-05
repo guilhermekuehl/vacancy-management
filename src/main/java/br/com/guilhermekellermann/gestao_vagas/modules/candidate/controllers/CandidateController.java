395cc49edb4661c2c410f1b4c933740afcdfd7be
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-06-05 20:31:21 -0300

    feat:Added company and job entity

--- src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,13 +2,10 @@ package br.com.guilhermekellermann.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.guilhermekellermann.gestao_vagas.exceptions.UserFoundException;
 import br.com.guilhermekellermann.gestao_vagas.modules.candidate.CandidateEntity;
-import br.com.guilhermekellermann.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.guilhermekellermann.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCases;
 import jakarta.validation.Valid;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
