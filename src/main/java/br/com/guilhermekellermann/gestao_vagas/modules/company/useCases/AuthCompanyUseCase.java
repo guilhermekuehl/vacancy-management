f6036c3d3368051a31023b92e5ee7a79898b4fea
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-06-19 19:31:30 -0300

    feat: JWT auth

--- src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,11 +1,18 @@
 package br.com.guilhermekellermann.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.guilhermekellermann.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.guilhermekellermann.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -13,16 +20,19 @@ import br.com.guilhermekellermann.gestao_vagas.modules.company.repositories.Comp
 @Service
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     @Autowired
     private CompanyRepository companyRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
     
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company =this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
             () -> {
-                throw new UsernameNotFoundException("Company not found");
+                throw new UsernameNotFoundException("Username/passwor incorrect");
             });
         // Verificar a senha são iguais
         var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
@@ -32,5 +42,11 @@ public class AuthCompanyUseCase {
             throw new AuthenticationException();
         }
         // Se for igual -> Gerar o token
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas")
+            .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+            .withSubject(company.getId().toString())
+            .sign(algorithm);
+        return token;
     }
 }
