7e4c858169f5b76aa2bc1784369a9d1142427367
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-06-26 21:58:27 -0300

    feat: creating candidate's token

--- src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -4,7 +4,11 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+    
+    Optional<CandidateEntity> findByUsername(String username);
 }
---
+++ src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.guilhermekellermann.gestao_vagas.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCandidateResponseDTO {
+    
+    private String access_token;
+}
