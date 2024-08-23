257fbbacf828d9509ba4ae80a27b6e8808b51c57
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-08-22 21:48:25 -0300

    refact: documentantion (Swagger)

--- src/main/java/br/com/guilhermekellermann/gestao_vagas/config/SwaggerConfig.java
+++ src/main/java/br/com/guilhermekellermann/gestao_vagas/config/SwaggerConfig.java
@@ -9,16 +9,18 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
-    
+
     @Bean
     public OpenAPI openAPI() {
 
         return new OpenAPI()
-        .info(new Info().title("Gestão de Vagas").description("API Responsável pela gestão de vagas").version("1"))
-        .schemaRequirement("jwt_auth", createSecurityScheme());
+                .info(new Info().title("Gestão de Vagas").description("API Responsável pela gestão de vagas")
+                        .version("1"))
+                .schemaRequirement("jwt_auth", createSecurityScheme());
     }
 
     private SecurityScheme createSecurityScheme() {
-        return new SecurityScheme().name("jwt_auth").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
+        return new SecurityScheme().name("jwt_auth").type(SecurityScheme.Type.HTTP).scheme("bearer")
+                .bearerFormat("JWT");
     }
 }
