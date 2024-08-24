bdfa3b225ab80fd24320a8b359d4b7c9851355b6
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-08-24 17:08:48 -0300

    feat: create apply job entity

--- src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import br.com.guilhermekellermann.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.guilhermekellermann.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.guilhermekellermann.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.guilhermekellermann.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import br.com.guilhermekellermann.gestao_vagas.modules.company.repositories.JobRepository;
 
 @Service
@@ -18,6 +19,9 @@ public class ApplyJobCandidateUseCase {
 
     @Autowired
     JobRepository jobRepository;
+
+    @Autowired
+    private ApplyJobRepository applyJobRepository;
 
     public void execute(UUID idCandidate, UUID idJob) {
         this.candidateRepository.findById(idCandidate)
