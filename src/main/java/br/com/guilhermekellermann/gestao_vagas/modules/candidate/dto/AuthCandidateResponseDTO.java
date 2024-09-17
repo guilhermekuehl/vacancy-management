cc4bf41e5f2d1b15946ea9fa9c285777bd3ce179
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-09-16 23:30:26 -0300

    feat: add token in SpringSecurity and on the session

--- src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
+++ src/main/java/br/com/guilhermekellermann/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -1,5 +1,7 @@
 package br.com.guilhermekellermann.gestao_vagas.modules.candidate.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,4 +15,5 @@ public class AuthCandidateResponseDTO {
     
     private String access_token;
     private Long expires_in;
+    private List<String> roles;
 }
